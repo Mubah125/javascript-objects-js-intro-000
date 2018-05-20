745beedddce54d267e04300888c3fe1e55276ac3
William Adu-Krow <mubah125@gmail.com>
2018-05-20 18:48:10 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -9,6 +9,6 @@ function updatePlaylist(playlist, artistName, songTitle){
 
 function removeFromPlaylist(playlist, artistName){
 
-return delete playlist.artistname;
-
+ delete playlist.artistname;
+return playlist;
 };
