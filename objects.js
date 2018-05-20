21283065de08202363d38b22d7aac30035b1ff18
William Adu-Krow <mubah125@gmail.com>
2018-05-20 18:39:37 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = {'Khalid': 'Location'};
 
 function updatePlaylist(playlist, name, song){
 
- Object.assign({} , playlist , { name: song })
-return playlist;
+ return Object.assign({} , playlist , { name: song })
+
 
 };
