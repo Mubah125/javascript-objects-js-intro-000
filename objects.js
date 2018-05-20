6fabcdb66a9e3086c75ec0ddbd513ea86f851ed5
William Adu-Krow <mubah125@gmail.com>
2018-05-20 18:51:06 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -9,5 +9,5 @@ function updatePlaylist(playlist, artistName, songTitle){
 
 function removeFromPlaylist(playlist, artistName){
 
-return delete playlist.artistname;
+return delete playlist.'Phil Ochs';
 };
